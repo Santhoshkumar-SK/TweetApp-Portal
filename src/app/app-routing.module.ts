56af4bb486d4bc6c65d0887af9914aec7de1bbcd
s k <santhoshkumarpln@gmail.com>
2022-07-26 20:37:06 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './authorization/forget-password/forget-password.component';
+import { LoginComponent } from './authorization/login/login.component';
+import { SignUpComponent } from './authorization/sign-up/sign-up.component';
+import { IndexPageComponent } from './twitter-console/index-page/index-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"forgetpassword",component:ForgetPasswordComponent},
+  {path:"signup",component:SignUpComponent},
+  {path:"tweet",component : IndexPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authorization/login/login.component';
+import { ForgetPasswordComponent } from './authorization/forget-password/forget-password.component';
+import { IndexPageComponent } from './twitter-console/index-page/index-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { SignUpComponent } from './authorization/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    IndexPageComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
