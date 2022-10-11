80d5870e593fa0805227eb88aefe962e262a03ea
Santhoshkumar <56360433+Santhoshkumar-SK@users.noreply.github.com>
2022-10-11 18:59:49 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverUrl : "http://localhost:44607/api/v1.0/tweets",  //"https://localhost:44323/api/v1.0/tweets", 
-  tokenKeyName : "425d9d6e-8d40-48d3-8f96-1617ac80acb2",
+  tokenKeyName : "",
   userapiUrl : "https://dh-tweetuserinfo-service.azurewebsites.net/api/UserInformation",
   tweetapiUrl : "https://dh-tweetinfo-service.azurewebsites.net/api/Tweets",
   authapiUrl : "https://dh-tweetuserinfo-service.azurewebsites.net/api/Authentication"
