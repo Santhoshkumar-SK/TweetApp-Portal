3ca84b08c620fa0aeafea389ec05eff291509157
Santhoshkumar <56360433+Santhoshkumar-SK@users.noreply.github.com>
2022-10-11 19:01:00 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   serverUrl : "http://localhost:44607/api/v1.0/tweets",  //"https://localhost:44323/api/v1.0/tweets", 
-  tokenKeyName : "425d9d6e-8d40-48d3-8f96-1617ac80acb2",
+  tokenKeyName : "",
   userapiUrl : "http://localhost:47535/api/UserInformation",
   tweetapiUrl : "http://localhost:26752/api/Tweets",
   authapiUrl : "http://localhost:47535/api/Authentication"
